862e8bda1e96160fbdd7bba20212838190d51353
William Bert <william@spanishdict.com>
2016-06-25 22:53:48 -0700

    Move and rename and meddle.

--- dev/bin/markdown-render.py
+++ dev/bin/markdown-render.py
@@ -11,32 +11,39 @@ TAIL = """
 """
 
 def main(args):
-    if len(args) < 3:
+    if len(args) < 2:
         print USAGE
         sys.exit(1)
+    if len(args) == 3:
+        css_dir = args[2].strip()
+        if css_dir.endswith("/"):
+            css_dir = css_dir[:-1]
+    else:
+        css_dir = "~/scm/vendor/markdown-css-themes/"
 
-    css_dir = args[2].strip()
-    if css_dir.endswith("/"):
-        css_dir = css_dir[:-1]
     head = """
-    <html>
-    <head>
+<html>
+  <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link href="%s/markdown-print.css" rel="stylesheet"></link>
-    </head>
-    <body>
-    """ % args[2]
+    <link href="%smarkdown-print.css" rel="stylesheet"></link>
+  </head>
+  <body>
+    """ % css_dir
 
     md_file = args[1].strip()
     md = open(md_file).read()
     marked = markdown.markdown(unicode(md, "utf8"))
     marked = unicode(head) + marked + unicode(TAIL)
+    marked = marked.encode("utf8")
 
     html_file = md_file.replace(".md", ".html")
-    with open(html_file, "w") as output:
-        output.write(marked.encode("utf8"))
+    try:
+        with open(html_file, "w") as output:
+            output.write(marked)
+    except IOError:
+        sys.stdout.write(marked)
 
-    print "Wrote to %s." % html_file
+    print "\nWrote to %s." % html_file
 
 if __name__ == "__main__":
     main(sys.argv)
